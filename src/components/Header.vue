0e653d0d2aa6de28d69b117ca6279129d1406794
Mogase <mogase@users.noreply.github.com>
2026-08-07 03:43:41 +0200

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,20 +11,39 @@
         <span></span><span></span><span></span>
       </button>
       <nav class="nav-links" :class="{ open: menuOpen }">
-        <a href="#overview">Overview</a>
-        <a href="#services">Services</a>
-        <a href="#projects">Projects</a>
-        <a href="#values">Values</a>
-        <a href="#contact" class="nav-cta">Contact</a>
+        <a href="#overview" @click="menuOpen = false">Overview</a>
+        <a href="#services" @click="menuOpen = false">Services</a>
+        <a href="#projects" @click="menuOpen = false">Projects</a>
+        <a href="#values" @click="menuOpen = false">Values</a>
+        <a href="#" class="nav-cta" @click.prevent="openModal">Contact</a>
       </nav>
     </div>
+
+    <ContactModal
+      :show="showModal"
+      email="mogasegase123@gmail.com"
+      phone-number="0673796372"
+      @close="showModal = false"
+    />
   </header>
 </template>
 
 <script>
+import ContactModal from './ContactModal.vue'
+
 export default {
+  components: { ContactModal },
   data() {
-    return { menuOpen: false }
+    return {
+      menuOpen: false,
+      showModal: false
+    }
+  },
+  methods: {
+    openModal() {
+      this.menuOpen = false
+      this.showModal = true
+    }
   }
 }
 </script>
